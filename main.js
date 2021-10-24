516bce24f4c0dfa3c72ae4c3ce90504cdeab88e1
Mariana Rodrigues <92376192+MAAARIAAANAAA@users.noreply.github.com>
2021-10-24 14:24:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'MAAARIAAANAAA',
+  youtube: 'mari9647',
+  facebook: 'Mariana Rodrigues',
+  instagram: 'maariiirodrigues',
+  twitter: 'marianapr1211'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userNameGitHub.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
